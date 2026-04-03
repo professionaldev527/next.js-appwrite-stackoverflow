96d1b2c538138cb7af4633d6f02f024cb4282cc3
Kaushik Adithya E <professionaldev527@gmail.com>
2026-04-03 07:14:56 +0530

    update 3

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import AnimatedGridPattern from "@/components/magicui/animated-grid-pattern";
-import { cn } from "@/utils/cn";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 const Footer = () => {
@@ -30,13 +30,15 @@ const Footer = () => {
         <footer className="relative block overflow-hidden border-t border-solid border-white/30 py-20">
             <div className="container mx-auto px-4">
                 <ul className="flex flex-wrap items-center justify-center gap-3">
-                    {items.map(item => (
+                    {items.map((item) => (
                         <li key={item.href}>
                             <Link href={item.href}>{item.title}</Link>
                         </li>
                     ))}
                 </ul>
-                <div className="mt-4 text-center">&copy; {new Date().getFullYear()} Riverpod</div>
+                <div className="mt-4 text-center">
+                    &copy; {new Date().getFullYear()} Riverpod
+                </div>
             </div>
             <AnimatedGridPattern
                 numSquares={30}
@@ -45,7 +47,7 @@ const Footer = () => {
                 repeatDelay={1}
                 className={cn(
                     "[mask-image:radial-gradient(3000px_circle_at_center,white,transparent)]",
-                    "inset-y-[-50%] h-[200%] skew-y-6"
+                    "inset-y-[-50%] h-[200%] skew-y-6",
                 )}
             />
         </footer>
